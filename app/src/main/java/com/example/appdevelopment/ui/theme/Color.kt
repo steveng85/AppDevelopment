fb8f00e10d5dc76d50ac2b0e599da0ab67a63880
pelle <pellehartvigandersen@gmail.com>
2022-10-09 12:46:27 +0200

    Initial component theme

--- app/src/main/java/com/example/appdevelopment/ui/theme/Color.kt
+++ app/src/main/java/com/example/appdevelopment/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.appdevelopment.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// LightColorScheme
+val White = Color(0xFFFFFFFF)
+val Blue = Color(0xFF007FFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// DarkColorScheme
+val Navy = Color(0xFF071A2F)
+val DarkBlue = Color(0xFF001E3C)
