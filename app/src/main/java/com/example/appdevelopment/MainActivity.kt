a48fd188e70812a10a5c472bb1670bfb59bd6c23
Whitebear <58033766+PelleWhitebear@users.noreply.github.com>
2022-10-13 19:46:48 +0200

    initial WelcomeScreen.kt

--- app/src/main/java/com/example/appdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/appdevelopment/MainActivity.kt
@@ -3,16 +3,8 @@ package com.example.appdevelopment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
-import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.appdevelopment.ui.theme.AppDevelopmentTheme
 
