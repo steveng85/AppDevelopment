3927bae3a589e84ed2e2a4780f03e8abcefbe76c
Steveng85 <s205456@student.dtu.dk>
2022-10-15 16:52:28 +0200

    Made ui for ForgotPassord view

--- app/src/main/java/com/example/appdevelopment/ui/screens/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/appdevelopment/ui/screens/ForgotPasswordScreen.kt
@@ -1,15 +1,18 @@
 package com.example.appdevelopment.ui.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.appdevelopment.navigation.Screen
@@ -18,19 +21,46 @@ import com.example.appdevelopment.navigation.Screen
 fun ForgotPasswordScreen(
     navController: NavController
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            modifier = Modifier.clickable {
-                navController.navigate(route = Screen.Welcome.route)
-            },
-            text = "ForgotPasswordScreen",
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = MaterialTheme.typography.displayLarge.fontSize,
-            fontWeight = FontWeight.Bold
-        )
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(35.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+
+            ) {
+            Text(
+                modifier = Modifier.clickable {
+                    navController.navigate(route = Screen.Welcome.route)
+                },
+                text = "Forgot your password?",
+                color = Color.Black,
+                fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                modifier = Modifier.padding(start = 100.dp, end = 100.dp),
+                text = "Write your e-mail below to reset your password",
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center)
+
+            Column(
+                modifier = Modifier
+                    .padding(
+                        start = 75.dp,
+                        end = 75.dp
+                    )
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(35.dp)
+            ) {
+
+                EmailBox()
+                ResetPasswordButton()
+            }
+        }
     }
 }
 
@@ -38,4 +68,13 @@ fun ForgotPasswordScreen(
 @Composable
 fun ForgotPasswordScreenPreview() {
     ForgotPasswordScreen(navController = rememberNavController())
+}
+
+@Composable
+fun ResetPasswordButton(){
+    DefaultButton(
+        onClick = "",
+        text = "Reset Password",
+        contentColor = Color.White,
+        containerColor = Color(0xFF007FFF))
 }
