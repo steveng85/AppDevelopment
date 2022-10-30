badea1bdcc0eac2a6111aa4078012ae22be793eb
Steveng85 <s205456@student.dtu.dk>
2022-10-30 19:16:24 +0100

    base ui and logik for LeaderboardsScreen.kt

---
+++ app/src/main/java/com/example/appdevelopment/mockData/leaderboards/Boards.kt
@@ -0,0 +1,34 @@
+package com.example.appdevelopment.mockData.leaderboards
+
+val boards = listOf(
+    Board(
+        1,
+        1,
+        "Steven",
+        14
+    ),
+    Board(
+        2,
+        2,
+        "Pelle",
+        10
+    ),
+    Board(
+        3,
+        3,
+        "Silke",
+        9
+    ),
+    Board(
+        4,
+        4,
+        "Erik",
+        8
+    ),
+    Board(
+        5,
+        5,
+        "Lotte",
+        2
+    ),
+)
