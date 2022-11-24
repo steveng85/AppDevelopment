6563a097da5407da0d073f0a74728a3f8659d690
Whitebear <58033766+PelleWhitebear@users.noreply.github.com>
2022-11-24 15:27:34 +0100

    Camera wo/ upload til firestore

---
+++ app/src/main/java/com/example/appdevelopment/data/CameraLogic.kt
@@ -0,0 +1,84 @@
+package com.example.appdevelopment.data
+
+import android.content.ContentValues
+import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.example.appdevelopment.data.domain.repository.CameraRepository
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.inject.Inject
+
+class CameraLogic @Inject constructor(
+    private val cameraProvider: ProcessCameraProvider,
+    private val cameraSelector: CameraSelector,
+    private val cameraPreview: Preview,
+    private val cameraImageAnalysis: ImageAnalysis,
+    private val cameraImageCapture: ImageCapture
+): CameraRepository {
+
+    override suspend fun onImageCaptureAndUpload(context: Context) {
+
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.ENGLISH)
+            .format(System.currentTimeMillis())
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, simpleDateFormat)
+            put(MediaStore.MediaColumns.MIME_TYPE,"image/jpeg")
+            if (Build.VERSION.SDK_INT > 28){
+                put(MediaStore.Images.Media.RELATIVE_PATH,"Pictures/My-Camera-App-Images")
+            }
+        }
+
+        val outputFileOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                context.contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+
+        cameraImageCapture.takePicture(
+            outputFileOptions,
+            ContextCompat.getMainExecutor(context),
+            object : ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(
+                        context,
+                        "Picture is posted!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        context,
+                        "There occurred an error when taking the picture. Try again.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        )
+
+    }
+
+    override suspend fun onImagePreview(previewView: PreviewView, lifecycleOwner: LifecycleOwner) {
+
+        cameraPreview.setSurfaceProvider(previewView.surfaceProvider)
+
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            cameraSelector,
+            cameraPreview,
+            cameraImageCapture,
+            cameraImageAnalysis
+        )
+    }
+}
