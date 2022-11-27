42a623039358ff0ae983175de1cc30c2d7d4eab1
Steveng85 <s205456@student.dtu.dk>
2022-11-27 21:57:01 +0100

    Changed structur for camera fire storage

--- app/src/main/java/com/example/appdevelopment/data/remote/repository/StorageRepositoryImpl.kt
+++ app/src/main/java/com/example/appdevelopment/data/remote/repository/StorageRepositoryImpl.kt
@@ -4,18 +4,52 @@ import android.net.Uri
 import com.example.appdevelopment.data.domain.repository.StorageRepository
 import com.example.appdevelopment.data.utils.await
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.URI
 import javax.inject.Inject
 
-//class StorageRepositoryImpl @Inject constructor(
-//    private val firebaseStorage: FirebaseStorage
-//): StorageRepository {
-//
-//    override suspend fun uploadPictureToStorage(): Uri {
-//        try {
-//            firebaseStorage.reference.child(TODO()).putFile(TODO())
-//                .await().storage.downloadUrl.await()
-//        }catch (e: Exception){
-//
-//        }
-//    }
-//}
+class StorageRepositoryImpl @Inject constructor(
+    private val firebaseStorage: FirebaseStorage
+): StorageRepository {
+
+    override fun uploadImageTask(uri: Uri?) {
+
+        /**
+         * Code snippets from Firebase docs:
+         * https://firebase.google.com/docs/storage/android/upload-files
+         **/
+
+        val storageRef = firebaseStorage.reference.child("images/image")
+
+        //withContext(Dispatchers.IO){
+
+                var file = uri
+                var uploadTask = file?.let { storageRef.putFile(it) }
+        if (uploadTask != null){
+            uploadTask.continueWithTask{ task ->
+                if (!task.isSuccessful){
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                storageRef.downloadUrl
+            }
+        }
+        //}
+
+    }
+
+    override suspend fun getUrl(): String {
+       val storageRef = firebaseStorage.reference.child("images")
+
+
+        return try {
+            storageRef.downloadUrl.toString()
+        }catch (e: Exception){
+            e.printStackTrace()
+            ""
+        }
+    }
+}
